097f62240dde496c533d6243debeae673ef5e37e
Tyler McHenry <tyler@nerdland.net>
2013-03-05 23:00:19 -0800

    Add ASIO Dispatcher

---
+++ src/asio-dispatcher.cc
@@ -0,0 +1,81 @@
+#include "asio-dispatcher.h"
+
+#include "boost/bind.hpp"
+#include "boost/thread/once.hpp"
+#include "boost/thread/thread.hpp"
+
+namespace polar_express {
+
+int AsioDispatcher::kNumWorkersPerService = 2;
+boost::shared_ptr<AsioDispatcher> AsioDispatcher::instance_;
+
+// static
+boost::shared_ptr<AsioDispatcher> AsioDispatcher::GetInstance() {
+  static once_flag once = BOOST_ONCE_INIT;
+  call_once(InitInstance, once);
+  return instance_;
+}
+
+AsioDispatcher::AsioDispatcher() {
+}
+
+AsioDispatcher::~AsioDispatcher() {
+}
+
+void AsioDispatcher::Start() {
+  work_.clear();
+  worker_threads_.reset(new thread_group);
+  cpu_io_service_ = StartService();
+  disk_io_service_ = StartService();
+  uplink_io_service_ = StartService();
+  downlink_io_service_ = StartService();
+  state_machine_io_service_ = StartService();
+}
+
+void AsioDispatcher::Stop() {
+  work_.clear();
+  worker_threads_->join_all();
+}
+
+void AsioDispatcher::PostCpuBound(boost::function<void()> callback) {
+  cpu_io_service_->post(callback);
+}
+
+void AsioDispatcher::PostDiskBound(boost::function<void()> callback) {
+  disk_io_service_->post(callback);
+}
+
+void AsioDispatcher::PostUplinkBound(boost::function<void()> callback) {
+  uplink_io_service_->post(callback);
+}
+
+void AsioDispatcher::PostDownlinkBound(boost::function<void()> callback) {
+  downlink_io_service_->post(callback);
+}
+
+void AsioDispatcher::PostStateMachine(boost::function<void()> callback) {
+  state_machine_io_service_->post(callback);
+}
+
+boost::shared_ptr<asio::io_service> AsioDispatcher::StartService() {
+  boost::shared_ptr<asio::io_service> io_service(new asio::io_service);
+  work_.push_back(boost::shared_ptr<asio::io_service::work>(
+      new asio::io_service::work(*io_service)));
+  for (int i = 0; i < kNumWorkersPerService; ++i) {
+    worker_threads_->create_thread(bind(&WorkerThread, io_service));
+  }
+  return io_service;
+}
+
+// static
+void AsioDispatcher::InitInstance() {
+  instance_.reset(new AsioDispatcher);
+}
+
+// static
+void AsioDispatcher::WorkerThread(
+    boost::shared_ptr<asio::io_service> io_service) {
+  io_service->run();
+}
+
+}  // namespace polar_express
---
+++ src/asio-dispatcher.h
@@ -0,0 +1,56 @@
+#ifndef ASIO_DISPATCHER_H
+#define ASIO_DISPATCHER_H
+
+#include "boost/asio.hpp"
+#include "boost/function.hpp"
+#include "boost/shared_ptr.hpp"
+
+#include "macros.h"
+
+namespace boost {
+class thread_group;
+}  // namespace boost
+
+namespace polar_express {
+
+class AsioDispatcher {
+ public:
+  static boost::shared_ptr<AsioDispatcher> GetInstance();
+
+  virtual ~AsioDispatcher();
+  
+  virtual void Start();
+  virtual void Stop();
+  
+  virtual void PostCpuBound(boost::function<void()> callback);
+  virtual void PostDiskBound(boost::function<void()> callback);
+  virtual void PostUplinkBound(boost::function<void()> callback);
+  virtual void PostDownlinkBound(boost::function<void()> callback);
+  virtual void PostStateMachine(boost::function<void()> callback);
+
+ private:
+  AsioDispatcher();
+
+  boost::shared_ptr<asio::io_service> StartService();
+  
+  static void InitInstance();
+  static void WorkerThread(boost::shared_ptr<asio::io_service> io_service);
+  
+  boost::shared_ptr<asio::io_service> cpu_io_service_;
+  boost::shared_ptr<asio::io_service> disk_io_service_;
+  boost::shared_ptr<asio::io_service> uplink_io_service_;
+  boost::shared_ptr<asio::io_service> downlink_io_service_;
+  boost::shared_ptr<asio::io_service> state_machine_io_service_;
+
+  vector<boost::shared_ptr<asio::io_service::work> > work_;
+  boost::shared_ptr<thread_group> worker_threads_;
+  
+  static int kNumWorkersPerService;
+  static boost::shared_ptr<AsioDispatcher> instance_;
+
+  DISALLOW_COPY_AND_ASSIGN(AsioDispatcher);
+};
+  
+}  // namespace polar_express
+
+#endif  // ASIO_DISPATCHER_H
