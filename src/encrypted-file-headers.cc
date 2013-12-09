42159c5faeeae1d8a24194dac59b9de902dffb14
Tyler McHenry <tyler@nerdland.net>
2013-12-08 22:16:12 -0800

    Define header formats for encrypted files.

---
+++ src/encrypted-file-headers.cc
@@ -0,0 +1,203 @@
+#include "encrypted-file-headers.h"
+
+#include <cassert>
+#include <cstring>
+
+#include <algorithm>
+
+namespace polar_express {
+namespace {
+
+// These four magic bytes at the beginning of a file help identify it as a Polar
+// Express encrypted bundle.
+const byte kMagic[4] = {'P', 'E', 'X', '\0'};
+
+const char kKeyDerivationTypeIdPbkdf2[] = "pbkdf2";
+const char kKeyDerivationTypeIdPexSha256Hkdf[] = "pex-sha256-hkdf";
+const char kEncryptionTypeIdAes256Cbf[] = "aes-256-cbf";
+const char kMacTypeIdSha256[] = "sha256";
+const char kMacTypeIdNone[] = "";
+
+template <int N>
+void SetTypeId(char (&type_id_field)[N], const char* type_id) {
+  assert(type_id != nullptr);
+  assert(std::strlen(type_id) < N);
+  std::fill(&type_id_field[0], &type_id_field[0] + N, '\0');
+  std::strcpy(&type_id_field[0], type_id);
+}
+
+}  // namespace
+
+// The generic header that describes the format of the remaining sections of
+// this file.
+//
+// Each part of the file (key derivation, encryption, MAC) has a type id string
+// and a format version number for its parameters header. When applicable, the
+// type IDs correspond to the ID used for the the same algorithm in the OpenSSL
+// command line utility. The parameters header version numbers allow future
+// versions of Polar Express to change the parameters header format for a type
+// in a backwards-compatible way.
+//
+// All format version numbers are 32-bit, and all type ID strings are 16 bytes,
+// nul-terminated and nul-padded.
+//
+// This Generic Header also has a format version. This struct is version 0.
+//
+// Currently supported type IDs:
+//   Key Derivation: "pbkdf2", "pex-sha256-hkdf"
+//   Encryption: "aes-256-cfb"
+//   MAC: "", "sha256"
+struct EncryptedFileHeaders::GenericHeaderFields {
+  const uint32_t generic_header_format_version = 0;
+  char key_derivation_type_id[16];
+  uint32_t key_derivation_parameters_format_version;
+  char encryption_type_id[16];
+  uint32_t encryption_parameters_format_version;
+  char mac_type_id[16];
+  uint32_t mac_parameters_format_version;
+} __attribute__((packed));
+
+// Parameters for PBKDF2 key derivation ("pbkdf2"). This struct is version 0.
+struct EncryptedFileHeaders::KeyDerivationParametersPbkdf2 {
+  uint32_t iteration_count;
+  byte encryption_key_salt[32];
+  byte mac_key_salt[32];
+} __attribute__((packed));
+
+// Parameters for HKDF key derivation using SHA256 HMAC ("pex-sha256-hkdf").
+// This name is prefixed with "pex-" to indicate that this used the HKDF
+// algorithm implemented inside Polar Express (on top of Crypto++ SHA256 HMAC)
+// since as of this writing Crypto++ does not have a native HKDF API. This
+// struct is version 0.
+struct EncryptedFileHeaders::KeyDerivationParametersPexSha256Hkdf {
+  byte encryption_key_salt[32];
+  byte mac_key_salt[32];
+} __attribute__((packed));
+
+// Parameters used for the AES 256 CBF-mode cipher. This struct is version 0.
+struct EncryptedFileHeaders::EncryptionParametersAes256Cbf {
+  byte initialization_vector[16];
+} __attribute__((packed));
+
+// There are no parameters for the null or SHA256 MAC types
+
+EncryptedFileHeaders::EncryptedFileHeaders()
+    : generic_header_fields_(new GenericHeaderFields({})),
+      key_derivation_parameters_pbkdf2_(new KeyDerivationParametersPbkdf2({})),
+      key_derivation_parameters_pex_sha256_hkdf_(
+          new KeyDerivationParametersPexSha256Hkdf({})),
+      encryption_parameters_aes256_cbf_(new EncryptionParametersAes256Cbf({})) {
+}
+
+EncryptedFileHeaders::~EncryptedFileHeaders() {
+}
+
+void EncryptedFileHeaders::SetKeyDerivationPbkdf2(
+    uint32_t iteration_count, const vector<byte>& encryption_key_salt,
+    const vector<byte>& mac_key_salt) {
+  assert(encryption_key_salt.size() ==
+         sizeof(EncryptedFileHeaders::KeyDerivationParametersPbkdf2::
+                    encryption_key_salt));
+  assert(mac_key_salt.size() ==
+         sizeof(EncryptedFileHeaders::KeyDerivationParametersPbkdf2::
+                mac_key_salt));
+
+  SetTypeId(generic_header_fields_->key_derivation_type_id,
+            kKeyDerivationTypeIdPbkdf2);
+  generic_header_fields_->key_derivation_parameters_format_version = 0;
+
+  *key_derivation_parameters_pbkdf2_ = {};
+  std::copy(encryption_key_salt.begin(), encryption_key_salt.end(),
+            &key_derivation_parameters_pbkdf2_->encryption_key_salt[0]);
+  std::copy(mac_key_salt.begin(), mac_key_salt.end(),
+            &key_derivation_parameters_pbkdf2_->mac_key_salt[0]);
+}
+
+void EncryptedFileHeaders::SetKeyDerivationPexSha256Hkdf(
+    const vector<byte>& encryption_key_salt, const vector<byte>& mac_key_salt) {
+  assert(encryption_key_salt.size() ==
+         sizeof(EncryptedFileHeaders::KeyDerivationParametersPexSha256Hkdf::
+                encryption_key_salt));
+  assert(mac_key_salt.size() ==
+         sizeof(EncryptedFileHeaders::KeyDerivationParametersPexSha256Hkdf::
+                mac_key_salt));
+
+  SetTypeId(generic_header_fields_->key_derivation_type_id,
+            kKeyDerivationTypeIdPexSha256Hkdf);
+  generic_header_fields_->key_derivation_parameters_format_version = 0;
+
+  *key_derivation_parameters_pex_sha256_hkdf_ = {};
+  std::copy(
+      encryption_key_salt.begin(), encryption_key_salt.end(),
+      &key_derivation_parameters_pex_sha256_hkdf_->encryption_key_salt[0]);
+  std::copy(mac_key_salt.begin(), mac_key_salt.end(),
+            &key_derivation_parameters_pex_sha256_hkdf_->mac_key_salt[0]);
+}
+
+void EncryptedFileHeaders::SetEncryptionAes256Cbf(
+    const vector<byte>& initialization_vector) {
+  assert(initialization_vector.size() ==
+         sizeof(EncryptedFileHeaders::EncryptionParametersAes256Cbf::
+                initialization_vector));
+
+  SetTypeId(generic_header_fields_->encryption_type_id,
+            kEncryptionTypeIdAes256Cbf);
+  generic_header_fields_->encryption_parameters_format_version = 0;
+
+  *encryption_parameters_aes256_cbf_ = {};
+  std::copy(initialization_vector.begin(), initialization_vector.end(),
+            &encryption_parameters_aes256_cbf_->initialization_vector[0]);
+}
+
+void EncryptedFileHeaders::SetMacSha256() {
+  SetTypeId(generic_header_fields_->mac_type_id, kMacTypeIdSha256);
+  generic_header_fields_->mac_parameters_format_version = 0;
+}
+
+void EncryptedFileHeaders::SetMacNone() {
+  SetTypeId(generic_header_fields_->mac_type_id, kMacTypeIdNone);
+  generic_header_fields_->mac_parameters_format_version = 0;
+}
+
+void EncryptedFileHeaders::GetHeaderBlock(vector<byte>* header_block) const {
+  assert(header_block != nullptr);
+
+  header_block->insert(header_block->end(), kMagic, kMagic + sizeof(kMagic));
+
+  header_block->insert(
+      header_block->end(),
+      reinterpret_cast<const byte*>(generic_header_fields_.get()),
+      reinterpret_cast<const byte*>(generic_header_fields_.get()) +
+          sizeof(*generic_header_fields_));
+
+  if (generic_header_fields_->key_derivation_type_id ==
+      kKeyDerivationTypeIdPbkdf2) {
+    header_block->insert(
+        header_block->end(),
+        reinterpret_cast<const byte*>(key_derivation_parameters_pbkdf2_.get()),
+        reinterpret_cast<const byte*>(key_derivation_parameters_pbkdf2_.get()) +
+            sizeof(*key_derivation_parameters_pbkdf2_));
+  } else if (generic_header_fields_->key_derivation_type_id ==
+             kKeyDerivationTypeIdPexSha256Hkdf) {
+    header_block->insert(
+        header_block->end(),
+        reinterpret_cast<const byte*>(
+            key_derivation_parameters_pex_sha256_hkdf_.get()),
+        reinterpret_cast<const byte*>(
+            key_derivation_parameters_pex_sha256_hkdf_.get()) +
+            sizeof(*key_derivation_parameters_pex_sha256_hkdf_));
+  }
+
+  if (generic_header_fields_->encryption_type_id ==
+      kEncryptionTypeIdAes256Cbf) {
+    header_block->insert(
+        header_block->end(),
+        reinterpret_cast<const byte*>(encryption_parameters_aes256_cbf_.get()),
+        reinterpret_cast<const byte*>(encryption_parameters_aes256_cbf_.get()) +
+            sizeof(*encryption_parameters_aes256_cbf_));
+  }
+
+  // None of the currently supported HMAC types require a parameters block.
+}
+
+}  // namespace polar_express
---
+++ src/encrypted-file-headers.h
@@ -0,0 +1,70 @@
+#ifndef ENCRYPTED_FILE_HEADERS_H
+#define ENCRYPTED_FILE_HEADERS_H
+
+#include <cinttypes>
+#include <memory>
+
+#include "macros.h"
+
+namespace polar_express {
+
+// The format of a Polar Express encrypted file is:
+//
+// 1. Magic bytes
+// 2. Generic Header
+// 3. Key Derivation parameters
+// 4. Encryption parameters
+// 5. MAC parameters
+// 6. Encrypted Data
+// 7. MAC (optional)
+//
+// The MAC may be omitted if, for example, a self-authenticating form of
+// encryption is being used. If a MAC is used, it should be computed over all
+// data in the file, including magic bytes and headers.
+//
+// This class defines and produces sections 1-5 above.
+//
+// TODO: This class should also be able to parse the headers that it generates.
+class EncryptedFileHeaders {
+ public:
+  EncryptedFileHeaders();
+  ~EncryptedFileHeaders();
+
+  // Salts must be exactly 32 bytes.
+  void SetKeyDerivationPbkdf2(uint32_t iteration_count,
+                              const vector<byte>& encryption_key_salt,
+                              const vector<byte>& mac_key_salt);
+
+  // Salts must be exactly 32 bytes.
+  void SetKeyDerivationPexSha256Hkdf(const vector<byte>& encryption_key_salt,
+                                     const vector<byte>& mac_key_salt);
+
+  // Initialization vector must be exactly 16 bytes.
+  void SetEncryptionAes256Cbf(const vector<byte>& initialization_vector);
+
+  void SetMacSha256();
+
+  void SetMacNone();
+
+  void GetHeaderBlock(vector<byte>* header_block) const;
+
+ private:
+  struct GenericHeaderFields;
+  struct KeyDerivationParametersPbkdf2;
+  struct KeyDerivationParametersPexSha256Hkdf;
+  struct EncryptionParametersAes256Cbf;
+
+  std::unique_ptr<GenericHeaderFields> generic_header_fields_;
+  std::unique_ptr<KeyDerivationParametersPbkdf2>
+      key_derivation_parameters_pbkdf2_;
+  std::unique_ptr<KeyDerivationParametersPexSha256Hkdf>
+      key_derivation_parameters_pex_sha256_hkdf_;
+  std::unique_ptr<EncryptionParametersAes256Cbf>
+      encryption_parameters_aes256_cbf_;
+
+  DISALLOW_COPY_AND_ASSIGN(EncryptedFileHeaders);
+};
+
+}  // namespace polar_express
+
+#endif  // ENCRYPTED_FILE_HEADERS_H
